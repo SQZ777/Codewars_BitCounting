812fa29a4896f1bcca03a2b3fc2be17dc69f001c
sqz777In91 <sqz777isme@gmail.com>
2018-01-09 17:30:06 +0800

    加入專案檔案。

---
+++ Codewars_BitCounting/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_BitCounting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
